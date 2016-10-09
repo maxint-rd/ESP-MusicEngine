0f292f4bb760dbd9365ebe2d6b0139045ceae932
Maxint R&D <mm_rd@maxint.nl>
2016-10-09 09:30:32 +0200

    Add files via upload

---
+++ examples/MusicEnginePlayOnce/MusicEnginePlayOnce.ino
@@ -0,0 +1,18 @@
+/*
+ * MusicEngine library for ESP8266 in Arduino IDE minimal example 
+ * by MMOLE (@maxint-rd)
+ * see https://github.com/maxint-rd/ESP-MusicEngine
+ */
+#include <MusicEngine.h>
+
+// Initialize MusicEngine object (buzzer between pin 14 and GND)
+MusicEngine music(14);
+
+void setup() {
+  // only play one tune
+  music.play("T180 L8 CDEC. r CDEC. r EFG. r EFG. r GAGFEC. r GAGFEC. r L4 C<A>C. r C<A>C.");
+}
+
+void loop() {
+  // nothing to do
+}
